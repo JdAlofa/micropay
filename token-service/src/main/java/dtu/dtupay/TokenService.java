1bad2b324f792a6b479852d0265c5201ce1c4903
shot-codes <nicolai.verbaarschot@gmail.com>
2024-01-18 17:58:55 +0100

    Added switch case in token service.

--- token-service/src/main/java/dtu/dtupay/TokenService.java
+++ token-service/src/main/java/dtu/dtupay/TokenService.java
@@ -11,25 +11,36 @@ import java.util.concurrent.CountDownLatch;
 
 public class TokenService {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		try (RabbitMQ rabbitMQ = new RabbitMQ()) {
 			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
 				String message = new String(delivery.getBody(), "UTF-8");
 				System.out.println(" [x] Received '" + message + "'");
-				// Process the received message as needed
+
 				ObjectMapper mapper = new ObjectMapper();
 				Event event = mapper.readValue(message, Event.class);
-				Token token = mapper.readValue(event.getPayload(), Token.class);
-				System.out.println("Received event: " + event);
-				System.out.println("Received token: " + token);
+				String eventType = event.getType();
+				switch (eventType) {
+					case "type1":
+						Token token = mapper.readValue(event.getPayload(), Token.class);
+						System.out.println("Received token: " + token);
+						break;
+
+					default:
+						System.out.println("Don't care about this type of event");
+						break;
+				}
 				// rabbitMQ.sendMessage(message);
 			};
 			rabbitMQ.setEventCallback(deliverCallback);
+
 			System.out.println("listening");
+
+			// Use latch to keep main running
 			CountDownLatch latch = new CountDownLatch(1);
 			latch.await();
 		} catch (Exception e) {
-			throw e;
+			System.out.println(e);
 		}
 	}
 }
